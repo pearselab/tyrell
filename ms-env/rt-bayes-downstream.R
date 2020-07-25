3566e6774bc932423ee540a70a4df4ee1750e17c
Will Pearse <will.pearse@gmail.com>
2020-07-25 09:32:43 +0100

    Ready for co-authors?

--- ms-env/rt-bayes-downstream.R
+++ ms-env/rt-bayes-downstream.R
@@ -11,11 +11,11 @@ meta <- meta[meta$NAME_0=="United States",]
 meta$code <- sapply(strsplit(meta$HASC_1, ".", fixed=TRUE), function(x) x[2])
 env <- env[match(names(processed_data$reported_deaths), meta$code),]
 pop <- log10(pop[match(names(processed_data$reported_deaths), meta$code)])
-env_time <- env[,34:148]
-sd.env <- sd(env_time, na.rm=TRUE)
+env <- env[,34:148]
+sd.env <- sd(env, na.rm=TRUE)
 sd.pop <- sd(pop, na.rm=TRUE)
 # Remove variables with same names (unnecessary, but done for clarity!)
-rm(env,pop,env_time,meta,processed_data)
+rm(env,pop,env,meta,processed_data)
 
 # Get raw coefficients and then back-transform, thne neaten and merge data
 load("imptf-models/covid19model-6.0/results/rt-bayes.Rdata")
@@ -34,63 +34,55 @@ data$r.env <- data$env/sd.env; data$r.pop <- data$pop/sd.pop
 #       posterior distributions of the coefficients aren't always going to
 #       be positive or negative (i.e., p!=1) and so you can have problems
 #       using logits
+# NOTE: using positive term for x*mob.coef because the X data are
+#       flipped (i.e., reductions in mobility are positive); discussed
+#       with Ettie
 inv.logit <- function(x) exp(x) / (exp(x)+1)
 logit <- function(x) log(x / (1-x))
-optim.func <- function(x, target, mob.coef, new.r0) return(abs(target - (new.r0*inv.logit(-(1-x)*mob.coef))))
+optim.func <- function(x, target, mob.coef, new.r0) return(abs(target - (2*new.r0*inv.logit(x*mob.coef))))
 optim.wrap <- function(target, mob.coef, new.r0){
-    output <- optim(0, optim.func, method="Brent", lower=-10, upper=10, target=target, mob.coef=mob.coef, new.r0=new.r0)
+    output <- optim(0, optim.func, method="Brent", lower=-10, upper=0, target=target, mob.coef=mob.coef, new.r0=new.r0)
     if(output$convergence != 0)
         stop("Something has gone wrong")
     return(output$par)
 }
-
-data$e.average <- data$e.transit <- data$e.residential <- data$p.average <- data$p.transit <- data$p.residential <- -999
-for(i in 1:50){
-    #for(i in seq_len(nrow(data))){
-    data$e.average[i] <- optim.wrap(1, data$average[i], 1+abs(data$r.env[i]))
-    data$e.transit[i] <- optim.wrap(1, data$transit[i], 1+abs(data$r.env[i]))
-    data$e.residential[i] <- optim.wrap(1, data$residential[i], 1+abs(data$r.env[i]))
-    data$p.average[i] <- optim.wrap(1, data$average[i], 1+abs(data$r.pop[i]))
-    data$p.transit[i] <- optim.wrap(1, data$transit[i], 1+abs(data$r.pop[i]))
-    data$p.residential[i] <- optim.wrap(1, data$residential[i], 1+abs(data$r.pop[i]))    
+data$e.average <- data$p.average <- -999 #data$e.transit <- data$e.residential <- data$p.average <- data$p.transit <- data$p.residential <- -999
+dc <- 2 # Assume 2 Degree Change
+pc <- 1 # Assume 1 log-unit Population Change (i.e., 10x)
+for(i in seq_len(nrow(data))){
+    data$e.average[i] <- optim.wrap(1, data$average[i], 1+abs(data$r.env[i]*dc))
+    #data$e.transit[i] <- optim.wrap(1, data$transit[i], 1+abs(data$r.env[i]*dc))
+    data$e.residential[i] <- optim.wrap(1, data$residential[i], 1+abs(data$r.env[i]*dc))
+    data$p.average[i] <- optim.wrap(1, data$average[i], 1+abs(data$r.pop[i]*pc))
+    #data$p.transit[i] <- optim.wrap(1, data$transit[i], 1+abs(data$r.pop[i]*pc))
+    data$p.residential[i] <- optim.wrap(1, data$residential[i], 1+abs(data$r.pop[i]*pc))    
 }
-    
+summary <- -apply(data[,c("p.residential","p.average","e.residential","e.average")], 2, quantile, prob=c(.05,.1,.25,.5,.75,.9,.95)) * 100
 
-
-summary <- apply(data, 2, quantile, prob=c(.025,.05,.1,.25,.5,.75,.9,.95,.975))
-summary <- summary[,order(summary[1,])]
-
-
-
-
-
-
-
-optim(1, optim.wrap, target=.1, mob.coef=1, method="Brent", lower=-10, upper=10)
-hist(logit(data$r.env/2) / data$transit)
-
-two.inv.logit()
-
-# Neaten data (reversing env for pretty plotting)
-data <- data.frame(
-    env, pop, average, transit, residential, r.env, r.pop
-)
-summary <- apply(data, 2, quantile, prob=c(.025,.05,.1,.25,.5,.75,.9,.95,.975))
-summary <- summary[,order(summary[1,])]
-summary[,"env"] <- -summary[,"env"]; summary[,"r.env"] <- -summary[,"r.env"]
-
-# Calculate relative 
-
-# Generate plot
+# Relative reduction plot
 pdf("us-bayes-posterior.pdf")
-cols <- c("red","red","black","black","black")
-dotchart(summary["50%",], xlim=c(0,2), pch=20, color=cols, pt.cex=4, font=2, frame.plot=FALSE, xlab="Relative importance", labels=c("Temperature","Population density", "Transit mobility", "Residential mobility", "Average mobility"))
-arrows(summary["25%",], 1:5, summary["75%",], length=0, lwd=10, col=cols)
-arrows(summary["10%",], 1:5, summary["90%",], length=0, lwd=5, col=cols)
-arrows(summary["2.5%",], 1:5, summary["97.5%",], length=0, lwd=1, col=cols)
+cols <- c("black","red","black","red")
+dotchart(summary["50%",], xlim=c(1,600), pch=20, color=cols, pt.cex=4, font=2, frame.plot=FALSE, xlab="Percent reduction needed in                       mobility to mitigate", labels=c("10x denser population","10x denser population","5°C cooler","5°C cooler"), log="x")
+mtext("                                              residential", side=1, line=2.5, font=2, adj=0)
+mtext("                                                 average", side=1, line=3.5, font=2, adj=0, col="red")
+arrows(summary["25%",], 1:4, summary["75%",], length=0, lwd=10, col=cols)
+arrows(summary["10%",], 1:4, summary["90%",], length=0, lwd=5, col=cols)
+arrows(summary["5%",], 1:4, summary["95%",], length=0, lwd=1, col=cols)
 dev.off()
 
-# Summary statistics in paper are:
-summary["50%",] # Coefficients
+# Generate paper summary statistics
+summary <- apply(data, 2, quantile, prob=c(.025,.05,.1,.25,.5,.75,.9,.95,.975))
+sink("ms-env/rt-bayes-downstream.txt")
+print("Posterior summaries:")
+summary["50%",]
+print("")
+print("P(env < 0):")
 (sum(data$env < 0) / nrow(data)) * 100 # Bayesian p-value
+print("")
+print("P(pop > 0):")
 (sum(data$pop > 0) / nrow(data)) * 100 # Bayesian p-value
+print("")
+print("Posterior correlations:")
+cor(data)
+print("")
+sink()
