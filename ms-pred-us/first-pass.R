a8049e73acfdb1572b48246079a7fcb829f60b8b
Will Pearse <will.pearse@gmail.com>
2020-09-22 21:52:36 +0100

    Hey Michael

--- ms-pred-us/first-pass.R
+++ ms-pred-us/first-pass.R
@@ -1,7 +1,11 @@
-load.jh <- function(filename, new.cases=FALSE, group.month=FALSE){
+# Headers
+source("src/packages.R")
+
+# Wrapper/worker functions
+load.jh <- function(filename, new.cases=FALSE, group.month=FALSE, thousands=TRUE){
     data <- read.csv(filename, as.is=TRUE)
     data$UID <- data$iso2 <- data$iso3 <- data$code3 <- data$FIPS <- data$Combined_Key <- NULL
-    browser()
+    
     if(group.month){
         month <- format(as.Date(names(data)[-1:-5], format="X%m.%d.%y"), "%m-%y")
         data <- data.frame(data[,1:5], t(apply(data[-1:-5], 1, function(x) tapply(x, month, sum))))
@@ -20,12 +24,40 @@ load.jh <- function(filename, new.cases=FALSE, group.month=FALSE){
 
         total <- with(data, tapply(cases, doy, sum))
         total <- data.frame(cases=total, doy=names(total), date=as.Date("2020-01-01")+as.numeric(names(total)))
-        
+
+        if(thousands){
+            total$cases <- total$cases/1000
+            data$cases <- data$cases/1000
+        }
         return(list(states=data, total=total))
     }
 }
+run.arima <- function(x, days, offset, min, max, width, ar=2, i=2, ma=0, log=FALSE, thousands=TRUE){
+    if(log)
+        x <- log10(x)
+    if(!thousands)
+        stop("Assumes data are formatted for AGORA")
+    model <- arima(x, order=c(ar, i, ma))
+    pred <- predict(model, n.ahead=days)
+    pred$pred <- pred$pred - offset
+    bin <- seq(min, max, width)
+    
+    return(data.frame(
+        prob=dnorm(bin, pred$pred[days], pred$se[days]),
+        bin=bin
+    ))
+}
 
+# Grab data
 c(states, total) %<-% load.jh("raw-data/cases/jh-us-confirmed.csv")
 c(d.states, d.total) %<-% load.jh("raw-data/cases/jh-us-confirmed.csv", new.cases=TRUE)
 
-with(d.states, plot(cases ~ doy, pch=20))
+# Run prediction (formatting as needed)
+curr.day <- total$date[nrow(total)]
+n.days <- as.numeric(as.Date("2020-09-30") - as.Date(curr.day))
+base <- total$cases[total$date=="2020-08-31"]
+arima.pred <- run.arima(total$cases, days=n.days, offset=base, min=0, max=4000, width=25)
+with(arima.pred, plot(prob ~ bin))
+
+arima.pred <- run.arima(total$cases, days=n.days+30, offset=base, min=0, max=4000, width=25)
+with(arima.pred, plot(prob ~ bin))
